d369555545865c5418448099f94cf2b16d9a3f53
ConnorFieldUser <conquest.field@gmail.com>
2017-02-16 17:33:33 -0500

    templates folder was in wrong spot, admin interface

--- ordering_system/admin.py
+++ ordering_system/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from ordering_system.models import Profile, MenuItem, Order, OrderItem
+
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(MenuItem)
+admin.site.register(OrderItem)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemInline,)
+
+admin.site.register(Order, OrderAdmin)
