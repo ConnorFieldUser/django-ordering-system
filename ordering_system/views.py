01207e47244c13ac3aaad15549d6f6773cf5b08b
ConnorFieldUser <conquest.field@gmail.com>
2017-02-17 15:34:58 -0500

    index page, login form, base.html, login reroute settings.

--- ordering_system/views.py
+++ ordering_system/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from django.views.generic import TemplateView
+
+from django.contrib.auth.forms import AuthenticationForm
 
 
 # Create your views here.
 
 
 class IndexView(TemplateView):
-        template_name = 'index.html'
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['login_form'] = AuthenticationForm
+        return context
